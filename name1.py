772510896528b1fce84361081469e57184a66fe7
pritam8250666100 <49182532+pritam8250666100@users.noreply.github.com>
2019-04-03 16:41:10 +0530

    Add files via upload

---
+++ name1.py
@@ -0,0 +1,31 @@
+##name="  pritam sharma  "
+##print(name.title())
+##print(name.strip())
+##print(name.find("g"))
+##print(name.replace("s","f"))
+
+##
+##x=input()
+##y=input()
+##print("before swap",x,y)
+##c=x
+##x=y
+##y=c
+##print("after swap",x,y)
+name="python"
+
+print(name.title())
+
+
+
+
+
+
+
+
+##print(name[-2])
+##print(len(name))
+
+##import datetime
+##d=datetime.datetime.now()
+##print(d)
---
+++ swap.py
@@ -0,0 +1,6 @@
+x=4
+y=5
+print("before swap",x,y)
+x, y=y, x
+print("after swap",x,y)
+
